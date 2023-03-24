4e1149471730bd70011135f130b7d12a086db23c
artemmm312 <artemmm312@gmail.com>
2023-03-24 16:53:08 +0300

    глава3, двенадцатая задача.

---
+++ chapter-3/task_12.php
@@ -0,0 +1,54 @@
+<?php
+
+//12. В массиве А(N,М) удалить столбцы, все элементы которых являются простыми числами.
+
+function isPrime(int $number): bool
+{
+	if ($number <= 1) {
+		return false;
+	}
+	for ($i = 2; $i <= sqrt($number); $i++) {
+		if ($number % $i === 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function anythingArray(array $arr): array
+{
+	$result = $arr;
+	$columns = [];
+	$count_N = count($arr);
+	$count_M = count($arr[0]);
+	for ($j = 0; $j < $count_M; $j++) {
+		for ($i = 0; $i < $count_N; $i++) {
+			if (isPrime($arr[$i][$j])) {
+				if ($i === $count_N - 1) {
+					$columns[] = $j;
+				}
+				continue;
+			}
+			break;
+		}
+	}
+	if (!empty($columns)) {
+		$n = count($columns);
+		for ($j = 0; $j < $n; $j++) {
+			for ($i = 0; $i < $count_N; $i++) {
+				unset($arr[$i][$columns[$j]]);
+			}
+		}
+	}
+	//return $columns;
+	return $arr;
+}
+
+$arr =
+	[
+		[5, 2, 3, 20],
+		[4, 3, 6, -15],
+		[7, 5, 4, 11],
+		[5, 7, 6, 9],
+	];
+print_r(anythingArray($arr));
